6bfc71cfc46436e8e00497a4e377718a636e44d5
Anthony Cruz <anthonyncruz@gmail.com>
2019-11-16 09:49:24 -0800

    add hero image and national park image assets

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/*
+NPS API Key: xBRqVrGeEgzB8HiOJy82A69FLrhMQgd5FSX9fIH0
+Hiking Project API Key: 7101314-2db36463e31e0bf91b2c49919876d9dc
+Weatherbit API Key: 221b43c97f2b4875a8d27a00c7c7d105
+*/
+
+/*
+Park Codes:
+Acadia National Park - acad
+National Park of American Samoa - npsa
+Arches National Park - arch
+Badlands National Park - badl
+Big Bend National Park - bibe
+Biscayne National Park - bisc
+Black Canyon Of The Gunnison National Park - blca
+Bryce Canyon National Park - brca
+Canyonlands National Park - cany
+Capitol Reef National Park - care
+Carlsbad Caverns National Park - cave
+Channel Islands National Park - chis
+Congaree National Park - cong
+Crater Lake National Park - crla
+Cuyahoga Valley National Park - cuva
+Death Valley National Park - deva 
+Denali National Park & Preserve - dena
+Dry Tortugas National Park - drto
+Everglades National Park - ever
+Gates Of The Arctic National Park & Preserve - gaar
+Gateway Arch National Park - jeff
+Glacier Bay National Park & Preserve - glba
+Glacier National Park - glac
+Grand Canyon National Park - grca
+Grand Teton National Park - grte
+Great Basin National Park - grba
+Great Sand Dunes National Park & Preserve - grsa
+Great Smoky Mountains National Park - grsm
+Guadalupe Mountains National Park - gumo
+Haleakal&#257; National Park - hale
+Hawai'i Volcanoes National Park - havo
+Hot Springs National Park - hosp
+Indiana Dunes National Park - indu
+Isle Royale National Park - isro
+Joshua Tree National Park - jotr
+Katmai National Park & Preserve - katm
+Kenai Fjords National Park - kefj
+Sequoia & Kings Canyon National Parks - seki
+Kobuk Valley National Park - kova
+Lake Clark National Park & Preserve - lacl
+Lassen Volcanic National Park - lavo
+Mammoth Cave National Park - maca
+Mesa Verde National Park - meve
+Mount Rainier National Park - mora
+North Cascades National Park - noca
+Olympic National Park - olym
+Petrified Forest National Park - pefo
+Pinnacles National Park - pinn
+Redwood National and State Parks - redw
+Rocky Mountain National Park - romo
+Saguaro National Park - sagu
+Shenandoah National Park - shen
+Theodore Roosevelt National Park - thro
+Virgin Islands National Park - viis
+Voyageurs National Park - voya
+Wind Cave National Park - wica
+Wrangell - St Elias National Park & Preserve - wrst
+Yellowstone National Park - yell
+Yosemite National Park - yose
+Zion National Park - zion
+*/
