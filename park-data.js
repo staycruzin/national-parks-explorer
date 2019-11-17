65f7a87471665c3d2ed6b8d1ae6e21a6c46b250f
Anthony Cruz <anthonyncruz@gmail.com>
2019-11-17 12:55:23 -0800

    created greybox html/css wireframe for home screen

---
+++ park-data.js
@@ -0,0 +1,242 @@
+const nationalParksData = [
+    {
+        parkCode: 'acad',
+        parkName: 'Acadia National Park'
+    },
+    {
+        parkCode: 'npsa',
+        parkName: 'National Park of American Samoa'
+    },
+    {
+        parkCode: 'arch',
+        parkName: 'Arches National Park'
+    },
+    {
+        parkCode: 'badl',
+        parkName: 'Badlands National Park'
+    },
+    {
+        parkCode: 'bibe',
+        parkName: 'Big Bend National Park'
+    },
+    {
+        parkCode: 'bisc',
+        parkName: 'Biscayne National Park'
+    },
+    {
+        parkCode: 'blca',
+        parkName: 'Black Canyon Of The Gunnison National Park'
+    },
+    {
+        parkCode: 'brca',
+        parkName: 'Bryce Canyon National Park'
+    },
+    {
+        parkCode: 'cany',
+        parkName: 'Canyonlands National Park'
+    },
+    {
+        parkCode: 'care',
+        parkName: 'Capitol Reef National Park'
+    },
+    {
+        parkCode: 'cave',
+        parkName: 'Carlsbad Caverns National Park'
+    },
+    {
+        parkCode: 'chis',
+        parkName: 'Channel Islands National Park'
+    },
+    {
+        parkCode: 'cong',
+        parkName: 'Congaree National Park'
+    },
+    {
+        parkCode: 'crla',
+        parkName: 'Crater Lake National Park'
+    },
+    {
+        parkCode: 'cuva',
+        parkName: 'Cuyahoga Valley National Park'
+    },
+    {
+        parkCode: 'deva',
+        parkName: 'Death Valley National Park'
+    },
+    {
+        parkCode: 'dena',
+        parkName: 'Denali National Park & Preserve'
+    },
+    {
+        parkCode: 'drto',
+        parkName: 'Dry Tortugas National Park'
+    },
+    {
+        parkCode: 'ever',
+        parkName: 'Everglades National Park'
+    },
+    {
+        parkCode: 'gaar',
+        parkName: 'Gates Of The Arctic National Park & Preserve'
+    },
+    {
+        parkCode: 'jeff',
+        parkName: 'Gateway Arch National Park'
+    },
+    {
+        parkCode: 'glba',
+        parkName: 'Glacier Bay National Park & Preserve'
+    },
+    {
+        parkCode: 'glac',
+        parkName: 'Glacier National Park'
+    },
+    {
+        parkCode: 'grca',
+        parkName: 'Grand Canyon National Park'
+    },
+    {
+        parkCode: 'grte',
+        parkName: 'Grand Teton National Park'
+    },
+    {
+        parkCode: 'grba',
+        parkName: 'Great Basin National Park'
+    },
+    {
+        parkCode: 'grsa',
+        parkName: 'Great Sand Dunes National Park & Preserve'
+    },
+    {
+        parkCode: 'grsm',
+        parkName: 'Great Smoky Mountains National Park'
+    },
+    {
+        parkCode: 'gumo',
+        parkName: 'Guadalupe Mountains National Park'
+    },
+    {
+        parkCode: 'hale',
+        parkName: 'Haleakal&#257; National Park'
+    },
+    {
+        parkCode: 'havo',
+        parkName: "Hawai'i Volcanoes National Park"
+    },
+    {
+        parkCode: 'hosp',
+        parkName: 'Hot Springs National Park '
+    },
+    {
+        parkCode: 'indu',
+        parkName: 'Indiana Dunes National Park'
+    },
+    {
+        parkCode: 'isro',
+        parkName: 'Isle Royale National Park'
+    },
+    {
+        parkCode: 'jotr',
+        parkName: 'Joshua Tree National Park'
+    },
+    {
+        parkCode: 'katm',
+        parkName: 'Katmai National Park & Preserve'
+    },
+    {
+        parkCode: 'kefj',
+        parkName: 'Kenai Fjords National Park'
+    },
+    {
+        parkCode: 'kova',
+        parkName: 'Kobuk Valley National Park'
+    },
+    {
+        parkCode: 'lacl',
+        parkName: 'Lake Clark National Park & Preserve'
+    },
+    {
+        parkCode: 'lavo',
+        parkName: 'Lassen Volcanic National Park'
+    },
+    {
+        parkCode: 'maca',
+        parkName: 'Mammoth Cave National Park'
+    },
+    {
+        parkCode: 'meve',
+        parkName: 'Mesa Verde National Park'
+    },
+    {
+        parkCode: 'mora',
+        parkName: 'Mount Rainier National Park'
+    },
+    {
+        parkCode: 'noca',
+        parkName: 'North Cascades National Park'
+    },
+    {
+        parkCode: 'olym',
+        parkName: 'Olympic National Park'
+    },
+    {
+        parkCode: 'pefo',
+        parkName: 'Petrified Forest National Park'
+    },
+    {
+        parkCode: 'pinn',
+        parkName: 'Pinnacles National Park'
+    },
+    {
+        parkCode: 'redw',
+        parkName: 'Redwood National and State Parks'
+    },
+    {
+        parkCode: 'romo',
+        parkName: 'Rocky Mountain National Park'
+    },
+    {
+        parkCode: 'sagu',
+        parkName: 'Saguaro National Park'
+    },
+    {
+        parkCode: 'seki',
+        parkName: 'Sequoia & Kings Canyon National Parks'
+    },
+    {
+        parkCode: 'shen',
+        parkName: 'Shenandoah National Park'
+    },
+    {
+        parkCode: 'thro',
+        parkName: 'Theodore Roosevelt National Park'
+    },
+    {
+        parkCode: 'viis',
+        parkName: 'Virgin Islands National Park'
+    },
+    {
+        parkCode: 'voya',
+        parkName: 'Voyageurs National Park'
+    },
+    {
+        parkCode: 'wica',
+        parkName: 'Wind Cave National Park'
+    },
+    {
+        parkCode: 'wrst',
+        parkName: 'Wrangell - St Elias National Park & Preserve'
+    },
+    {
+        parkCode: 'yell',
+        parkName: 'Yellowstone National Park'
+    },
+    {
+        parkCode: 'yose',
+        parkName: 'Yosemite National Park'
+    },
+    {
+        parkCode: 'zion',
+        parkName: 'Zion National Park'
+    } 
+];
