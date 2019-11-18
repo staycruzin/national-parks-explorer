fbfdf2620043d646c396c4c6eab10a42502df6d7
Anthony Cruz <anthonyncruz@gmail.com>
2019-11-18 07:23:30 -0800

    implemented Fetch API to load data for getParkDetail(), getParkWeather(), getHikingTrails(), and getCampgrounds() methods

--- script.js
+++ script.js
@@ -1,19 +1,165 @@
 'use strict';
 
-/*
-NPS API Key: xBRqVrGeEgzB8HiOJy82A69FLrhMQgd5FSX9fIH0
-Hiking Project API Key: 7101314-2db36463e31e0bf91b2c49919876d9dc
-Weatherbit API Key: 221b43c97f2b4875a8d27a00c7c7d105
-*/
+const parksArray = Array.from(nationalParksData);
 
-const PARK_DATA = Array.from(nationalParksData);
+const NPS_URL = 'https://developer.nps.gov/api/v1/';
+const HIKING_PROJECT_URL = 'https://www.hikingproject.com/data/';
+const WEATHERBIT_URL = 'https://api.weatherbit.io/v2.0/forecast/daily';
 
-function displayParkDetail() {
+const NPS_API_KEY = 'xBRqVrGeEgzB8HiOJy82A69FLrhMQgd5FSX9fIH0';
+const HIKING_PROJECT_API_KEY = '7101314-2db36463e31e0bf91b2c49919876d9dc';
+const WEATHERBIT_API_KEY = '221b43c97f2b4875a8d27a00c7c7d105';
 
+// Need to implement error handling code in getParkDetail catch block instead of just a console log
+// Figure out a way to get rid of the duplicate "parseLatitudeAndLongitude" calls?
+
+function formatQueryParams(params) {
+    const queryString = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+
+    return queryString.join('&');
+}
+
+function parseLatitudeandLongitude(latLong) {
+    let latLongParsed = latLong.replace(/lat:|long:/g, '').split(', ');
+
+    return latLongParsed;
+}
+
+function displayCampgrounds(responseJson) {
+    console.log(responseJson);
+}
+
+function getCampgrounds(currentParkCode) {
+    const params = {
+        parkCode: currentParkCode,
+        api_key: NPS_API_KEY
+    }
+
+    const queryString = formatQueryParams(params);
+    const url = NPS_URL + 'campgrounds?' + queryString;
+    
+    console.log(url);
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => {
+            displayCampgrounds(responseJson);
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
+}
+
+function displayHikingTrails(responseJson) {
+    console.log(responseJson);
+}
+
+function getHikingTrails(latLong) {
+    let latLongParsed = parseLatitudeandLongitude(latLong);
+    let latitude = latLongParsed[0];
+    let longitude = latLongParsed[1];
+
+    const params = {
+        lat: latitude,
+        lon: longitude,
+        key: HIKING_PROJECT_API_KEY
+    }
+
+    const queryString = formatQueryParams(params);
+    const url = HIKING_PROJECT_URL + 'get-trails?' + queryString;
+    
+    console.log(url);
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => {
+            displayHikingTrails(responseJson);
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
+}
+
+function displayWeather(responseJson) {
+    console.log(responseJson);
+}
+
+function getParkWeather(latLong) {
+    let latLongParsed = parseLatitudeandLongitude(latLong);
+    let latitude = latLongParsed[0];
+    let longitude = latLongParsed[1];
+
+    const params = {
+        lat: latitude,
+        lon: longitude,
+        units: 'I',
+        days: 5,
+        key: WEATHERBIT_API_KEY
+    }
+
+    const queryString = formatQueryParams(params);
+    const url = WEATHERBIT_URL + '?' + queryString;
+
+    console.log(url);
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => {
+            displayWeather(responseJson);
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
+}
+
+function displayParkDetail(responseJson) {
+    console.log(responseJson);
 }
 
 function getParkDetail(parkCodeSelected) {
-    console.log(parkCodeSelected);
+    const params = {
+        parkCode: parkCodeSelected,
+        api_key: NPS_API_KEY
+    }
+
+    const queryString = formatQueryParams(params);
+    const url = NPS_URL + 'parks?' + queryString;
+    let latLong;
+
+    console.log(url);
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => {
+            latLong = responseJson.data[0].latLong;
+
+            displayParkDetail(responseJson);
+            getParkWeather(latLong);
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
 }
 
 function watchForm() {
@@ -37,11 +183,11 @@ function initializeSelect2() {
 function populateDropdown() {
     let parksList = document.getElementById("parks-list");
 
-    for (let i = 0; i < PARK_DATA.length; i++) {
+    for (let i = 0; i < parksArray.length; i++) {
         let option = document.createElement("option");
 
-        option.innerHTML = PARK_DATA[i].parkName;
-        option.value = PARK_DATA[i].parkCode;
+        option.innerHTML = parksArray[i].parkName;
+        option.value = parksArray[i].parkCode;
 
         parksList.appendChild(option);
     }
