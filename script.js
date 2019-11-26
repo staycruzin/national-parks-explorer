38d410ee645fbca2252a1be6da8f9ed06f3d8f80
Anthony Cruz <anthonyncruz@gmail.com>
2019-11-26 07:26:03 -0800

    refactored index.html, style.css and made minor style changes

--- script.js
+++ script.js
@@ -34,9 +34,9 @@ function parseLatitudeandLongitude(latLong) {
 function backButtonClicked() {
     
     $( ".js-back-button" ).click(function() {
-        $('.hiking-content').addClass('hidden');
-        $('.campground-content').addClass('hidden');
-        $('.detail-content').removeClass('hidden');
+        $('#hiking-content').addClass('hidden');
+        $('#campground-content').addClass('hidden');
+        $('#detail-content').removeClass('hidden');
         window.scrollTo(0, 0);
     });
 }
@@ -49,23 +49,23 @@ function displayCampgrounds(responseJson) {
 
     if (responseJson.data.length === 0) {
         $('#campgrounds-list').append(`
-        <li>
-            <h3 class="center-h3">No campgrounds found!</h3>
+        <li class="center">
+            <h3>No campgrounds found!</h3>
         </li>
     `);     
     }
     
     for (let i = 0; i < responseJson.data.length; i++) {
         $('#campgrounds-list').append(`
-            <li>
+            <li class="center">
                 <h3>${responseJson.data[i].name}</h3>
                 <p>${responseJson.data[i].description}</p>
             </li>
         `);
     }
 
-    $('.detail-content').addClass('hidden');
-    $('.campground-content').removeClass('hidden');
+    $('#detail-content').addClass('hidden');
+    $('#campground-content').removeClass('hidden');
 }
 
 function getCampgrounds(currentParkCode) {
@@ -109,15 +109,15 @@ function displayHikingTrails(responseJson) {
 
     if (responseJson.trails.length === 0) {
         $('#trails-list').append(`
-        <li>
-            <h3 class="center-h3">No hiking trails found!</h3>
+        <li class="center">
+            <h3>No hiking trails found!</h3>
         </li>
     `);     
     }
     
     for (let i = 0; i < responseJson.trails.length; i++) {
         $('#trails-list').append(`
-            <li>
+            <li class="center">
                 <h3>${responseJson.trails[i].name}</h3>
                 <p>${responseJson.trails[i].summary}</p>
                 <a target="_blank" href="${responseJson.trails[i].url}">Click here to learn more!</a>
@@ -125,8 +125,8 @@ function displayHikingTrails(responseJson) {
         `);
     }
 
-    $('.detail-content').addClass('hidden');
-    $('.hiking-content').removeClass('hidden');
+    $('#detail-content').addClass('hidden');
+    $('#hiking-content').removeClass('hidden');
 }
 
 function getHikingTrails(latLong) {
@@ -231,55 +231,55 @@ function getParkWeather(latLong) {
 function displayParkDetail(responseJson) {
     console.log(responseJson);
 
-    $('.detail-content').empty();
+    $('#detail-content').empty();
     
-    $('.detail-content').append(`        
-        <h2>${responseJson.data[0].fullName}</h2>
-        <section class="park-image-container detail-section">
+    $('#detail-content').append(`        
+        <h2 class="merriweather center">${responseJson.data[0].fullName}</h2>
+        <section id="park-image-container" class="detail-section">
             <img class="park-image" src="assets/park-images/${responseJson.data[0].parkCode}.jpg" alt="A picture of ${responseJson.data[0].fullName}"/>
         </section>
 
-        <section class="basic-details detail-section">
+        <section id="basic-details" class="detail-section">
             <div class="state">
-                <h3>State(s)</h3>
+                <h3 class="merriweather">State(s)</h3>
                 <p>${responseJson.data[0].states}</p>
             </div>
             <div class="description">
-                <h3>Description</h3>
+                <h3 class="merriweather">Description</h3>
                 <p>${responseJson.data[0].description}</p>
             </div>
         </section>
 
-        <section class="weather-details detail-section">
+        <section id="weather-details" class="detail-section">
             <div class="weather-forecast">
-                <h3>Five Day Forecast</h3>
+                <h3 class="merriweather">Five Day Forecast</h3>
                 <div class="daily-weather-container detail-section">
                 </div>
             </div>
             <br>
-            <h3>Weather Information</h3>
+            <h3 class="merriweather">Weather Information</h3>
             <p>${responseJson.data[0].weatherInfo}</p> 
         </section>
 
-        <section class="directions-and-more detail-section">
+        <section id="directions-and-more" class="detail-section">
             <div>
-                <h3>Directions Information</h3>
+                <h3 class="merriweather">Directions Information</h3>
                 <p>${responseJson.data[0].directionsInfo} More info <a target="_blank" href="${responseJson.data[0].directionsUrl}">here</a>.</p>
             </div>
 
             <div class="buttons-container">
-                <button class="js-camp-button detail-button" type="button">Campgrounds</button>
-                <button class="js-hike-button detail-button" type="button">Hiking Trails</button>
+                <button class="js-camp-button detail-button button" type="button">Campgrounds</button>
+                <button class="js-hike-button detail-button button" type="button">Hiking Trails</button>
             </div>
         </section>
     `);
 
-    $('.hero-home-screen').addClass('hidden');
-    $('.home-content').addClass('hidden');
-    $('.hiking-content').addClass('hidden');
-    $('.campground-content').addClass('hidden');
-    $('.detail-screen-heading').removeClass('hidden');
-    $('.detail-content').removeClass('hidden');
+    $('#hero-home-screen').addClass('hidden');
+    $('#home-content').addClass('hidden');
+    $('#hiking-content').addClass('hidden');
+    $('#campground-content').addClass('hidden');
+    $('#detail-screen-heading').removeClass('hidden');
+    $('#detail-content').removeClass('hidden');
 }
 
 function getParkDetail(parkCodeSelected) {
@@ -318,9 +318,9 @@ function getParkDetail(parkCodeSelected) {
 // Collapses the Nav when it is open and the user clicks off of it.
 function clickedOffNav() {
     $(document).click(function(e) {
-         if (viewportWidth <= 1199 && $(".search").is(':visible')) {
-              if ($(e.target).closest(".search").length === 0 && $(e.target).closest(".menu-button").length === 0) {
-                   $( ".search" ).slideToggle( "slow");
+         if (viewportWidth <= 1199 && $("#search").is(':visible')) {
+              if ($(e.target).closest("#search").length === 0 && $(e.target).closest(".menu-button").length === 0) {
+                   $( "#search" ).slideToggle( "slow");
                    $( ".close-button" ).hide();
                    $( ".open-button" ).show();
               }     
@@ -331,7 +331,7 @@ function clickedOffNav() {
 // Collapses the nav when the close menu button is clicked.
 function closeMenuClicked() {
     $( ".close-button" ).click(function() {
-         $( ".search" ).slideToggle( "slow");
+         $( "#search" ).slideToggle( "slow");
          $( ".close-button" ).hide();
          $( ".open-button" ).show();
     });
@@ -340,7 +340,7 @@ function closeMenuClicked() {
 // Expands the nav when the open menu button is clicked.
 function openMenuClicked() {
     $( ".open-button" ).click(function() {
-         $( ".search" ).slideToggle( "slow");
+         $( "#search" ).slideToggle( "slow");
          $( ".open-button" ).hide();
          $( ".close-button" ).show();
     });
@@ -351,9 +351,9 @@ function setNavBar() {
     if (viewportWidth <= 1199) {
          $(".open-button").show();
          $(".close-button").hide();
-         $(".search").hide();
+         $("#search").hide();
     } else {
-         $(".search").show();
+         $("#search").show();
          $(".open-button").hide();
          $(".close-button").hide();
     }
@@ -373,13 +373,13 @@ function setViewportWidth() {
 }
 
 function watchForms() {
-    $('.js-parks-form-home').submit(event => {
+    $('#js-parks-form-home').submit(event => {
         event.preventDefault();
         const parkCodeSelected = $("#parks-list-home").val();
         
         if (parkCodeSelected !== "") {
             if (viewportWidth <= 1199) {
-                $( ".search" ).hide();
+                $( "#search" ).hide();
                 $( ".close-button" ).hide();
                 $( ".open-button" ).show();
             }
@@ -388,12 +388,12 @@ function watchForms() {
         }
     });
 
-    $('.js-parks-form-detail').change(function() {
+    $('#js-parks-form-detail').change(function() {
         const parkCodeSelected = $("#parks-list-detail").val();
 
         if (parkCodeSelected !== "" && parkCodeSelected !== null) {
             if (viewportWidth <= 1199) {
-                $( ".search" ).slideToggle( "fast");
+                $( "#search" ).slideToggle( "fast");
                 $( ".close-button" ).hide();
                 $( ".open-button" ).show();
             }
@@ -435,13 +435,13 @@ $('#home-link').click(function(event){
     $("#parks-list-home").select2("val", "0");
     $("#parks-list-detail").select2("val", "0");
 
-    $('.hiking-content').addClass('hidden');
-    $('.campground-content').addClass('hidden');
-    $('.detail-content').addClass('hidden');
-    $('.detail-screen-heading').addClass('hidden');
+    $('#hiking-content').addClass('hidden');
+    $('#campground-content').addClass('hidden');
+    $('#detail-content').addClass('hidden');
+    $('#detail-screen-heading').addClass('hidden');
 
-    $('.hero-home-screen').removeClass('hidden');
-    $('.home-content').removeClass('hidden');
+    $('#hero-home-screen').removeClass('hidden');
+    $('#home-content').removeClass('hidden');
     window.scrollTo(0, 0);
 });
 
